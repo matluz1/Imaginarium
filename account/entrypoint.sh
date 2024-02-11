d4dda8de94a01b9c61978e537d462cb9947981b8
erenes <matluz994@gmail.com>
2024-02-11 18:59:26 -0300

    add prisma name starter

--- account/entrypoint.sh
+++ account/entrypoint.sh
@@ -2,9 +2,9 @@
 /wait-for-it.sh account-db:5432 -t 60
 npx prisma generate
 if [ "${MODE}" = "DEV" ]; then 
-  npx prisma migrate dev
+  npx prisma migrate dev --name init
   npx nodemon src/index
 elif [ "${MODE}" = "PROD" ]; then
-  npx prisma migrate deploy
+  npx prisma migrate deploy --name init
   node src/index
 fi
